6da2a6d1117c0bb7cd8c247576191974a9167d1f
Zachary Rojas <ztr2194@gmail.com>
2023-03-09 12:43:23 -0600

    event listeners lab done

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+function addingEventListener() { 
+    alert('I was clicked!');
+    input.addEventListener('click', alert);
 }
